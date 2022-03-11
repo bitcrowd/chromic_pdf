e826133c8b957250dc5cce6d92d220f77abdf11d
William Fank Thome <willegp88@gmail.com>
2022-03-10 21:14:15 -0300

    feat: page sizes

--- lib/chromic_pdf/template.ex
+++ lib/chromic_pdf/template.ex
@@ -32,7 +32,20 @@ defmodule ChromicPDF.Template do
           | {:header, blob()}
           | {:footer, blob()}
 
-  @type paper_size :: :a4 | :us_letter | {float(), float()}
+  @type paper_size ::
+          {float(), float()}
+          | :a0
+          | :a1
+          | :a2
+          | :a3
+          | :a4
+          | :a5
+          | :a6
+          | :a7
+          | :a8
+          | :a9
+          | :a10
+          | :us_letter
 
   @type style_option ::
           {:size, paper_size()}
@@ -45,7 +58,17 @@ defmodule ChromicPDF.Template do
           | {:webkit_print_color_adjust, binary()}
 
   @paper_sizes_in_inch %{
+    a0: {33.1, 46.8},
+    a1: {23.4, 33.1},
+    a2: {16.5, 23.4},
+    a3: {11.7, 16.5},
     a4: {8.3, 11.7},
+    a5: {5.8, 8.3},
+    a6: {4.1, 5.8},
+    a7: {2.9, 4.1},
+    a8: {2.0, 2.9},
+    a9: {1.5, 2.0},
+    a10: {1.0, 1.5},
     us_letter: {8.5, 11.0}
   }
 
