9f9003ca23a791a7bef654292bea35ab61f167e4
Malte Rohde <malte@bitcrowd.net>
2022-03-17 10:27:25 +0100

    lock cowboy on 2.8.0

--- mix.exs
+++ mix.exs
@@ -80,7 +80,10 @@ defmodule ChromicPdf.MixProject do
       {:phoenix, "~> 1.5", only: [:dev, :integration]},
       {:phoenix_html, "~> 2.14", only: [:dev, :integration]},
       {:plug, "~> 1.11", only: [:dev, :integration]},
-      {:plug_cowboy, "~> 2.4", only: [:integration]}
+      {:plug_cowboy, "~> 2.4", only: [:integration]},
+
+      # NOTE: cowboy 2.9.0 fails with ranch error
+      {:cowboy, "< 2.9.0", only: [:integration]}
     ]
   end
 
