349c19a37d6afc0d26ad023dd13e8ee636510788
Girish Vadlamudi <114484179+VadlamudiGirishVrag@users.noreply.github.com>
2024-02-26 14:03:15 +0100

    Updated mix.exs
    - Downgraded the phoenix_html version from 3.3 to 3.2

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule ChromicPdf.MixProject do
       {:nimble_pool, "~> 0.2 or ~> 1.0"},
       {:plug, "~> 1.11", optional: true},
       {:plug_crypto, "~> 1.2 or ~> 2.0", optional: true},
-      {:phoenix_html, "~> 2.14 or ~> 3.3 or ~> 4.0", optional: true},
+      {:phoenix_html, "~> 2.14 or ~> 3.2 or ~> 4.0", optional: true},
       {:telemetry, "~> 0.4 or ~> 1.0"},
       {:websockex, ">= 0.4.3", optional: true},
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
